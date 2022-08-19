eb7650b3004e5aa5bcf1bd3cbeb697a736d35cd8
cw692 <101645970+wcy692@users.noreply.github.com>
2022-08-19 08:47:48 +0100

    update dbConn.php to connect to cleardb

--- php/dbConn.php
+++ php/dbConn.php
@@ -1,9 +1,17 @@
 <?php
 
   function pdoConn(){
-    $dsn = 'mysql:host=localhost;dbname=griefspace';
-    $username = 'root';
-    $pwd = '';
+    // $dsn = 'mysql:host=localhost;dbname=griefspace';
+    // $username = 'root';
+    // $pwd = '';
+    $url = parse_url(getenv("mysql://bc5b470f47717d:5bce9b98@eu-cdbr-west-03.cleardb.net/heroku_747f91e40ccaa1d?reconnect=true"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    $dsn = "mysql:host=$server;dbname=$db";
+
     try {
         return $dbh = new PDO($dsn, $username, $pwd);
       } catch (PDOException $e) {
