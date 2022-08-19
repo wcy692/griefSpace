219882ed39b72c862f8d523ab6ef4d7a552704bf
cw692 <101645970+wcy692@users.noreply.github.com>
2022-08-19 11:13:22 +0100

    get master

--- php/dbConn.php
+++ php/dbConn.php
@@ -1,17 +1,9 @@
 <?php
 
   function pdoConn(){
-    $dsn = 'mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_747f91e40ccaa1d';
-    $username = 'bc5b470f47717d';
-    $pwd = '5bce9b98';
-    // $url = parse_url(getenv("mysql://bc5b470f47717d:5bce9b98@eu-cdbr-west-03.cleardb.net/heroku_747f91e40ccaa1d?reconnect=true"));
-    //
-    // $server = $url["host"];
-    // $username = $url["user"];
-    // $password = $url["pass"];
-    // $db = substr($url["path"], 1);
-    // $dsn = "mysql:host=$server;dbname=$db";
-
+    $dsn = 'mysql:host=localhost;dbname=griefspace';
+    $username = 'root';
+    $pwd = '';
     try {
         return $dbh = new PDO($dsn, $username, $pwd);
       } catch (PDOException $e) {
